a598416d74175026455e3f2b010c49087ad5bdbb
Joe <joe.melsha@live.com>
2025-12-20 20:45:07 -0600

    Windows install script

---
+++ install.ps1
@@ -0,0 +1,14 @@
+#Requires -Version 5.1
+
+param([string]$Mode = "release")
+$DeployFolder = "C:\Program Files\Rojo"
+
+Set-Location $PSScriptRoot
+cargo build "--$Mode"
+& ".\target\$Mode\rojo.exe" build plugin.project.json --plugin Rojo.rbxm
+if ($LASTEXITCODE -ne 0) { throw "Plugin build failed" }
+gsudo {
+  Stop-Process -Name "rojo" -Force -ErrorAction SilentlyContinue
+  Remove-Item "$DeployFolder\*" -Recurse -Force
+  Copy-Item ".\target\$Mode\rojo.exe" "$DeployFolder\"
+}
