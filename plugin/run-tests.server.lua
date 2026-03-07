9ed8cf3f3776e2c36ef36ee12caf1fd56dde8806
Joe <joe.melsha@live.com>
2026-03-07 14:17:32 -0600

    Refactor run-tests.server.lua to improve package loading and update subproject dependencies
    - Changed the way TestEZ and Rojo are required by using WaitForChild for better reliability.
    - Updated subproject commits for Flipper, Promise, Roact, and TestEZ to their latest versions.

--- plugin/run-tests.server.lua
+++ plugin/run-tests.server.lua
@@ -1,8 +1,9 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
-local TestEZ = require(ReplicatedStorage.Packages:WaitForChild("TestEZ", 10))
+local Packages = ReplicatedStorage:WaitForChild("Packages", 10)
+local TestEZ = require(Packages:WaitForChild("TestEZ", 10))
 
-local Rojo = ReplicatedStorage.Rojo
+local Rojo = ReplicatedStorage:WaitForChild("Rojo", 10)
 
 local Settings = require(Rojo.Plugin.Settings)
 Settings:set("logLevel", "Trace")
